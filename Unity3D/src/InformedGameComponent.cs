6f24a3a6009a63923d115ae50af7c0d0a86ab670
WazzaMLD <wmolloy@gmail.com>
2015-08-12 19:56:35 +1000

    Added Unity3D sub-project.

---
+++ Unity3D/src/InformedGameComponent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using UnityEngine;
+
+namespace Lokel.CoolFramework.Unity3D {
+
+    /// <summary>
+    /// The InformedGameComponent is a MonoBehaviour and also a MessageSubscriber
+    /// for use with the middleware in Lokel.CoolFramework.
+    /// </summary>
+    public class InformedGameComponent : MonoBehaviour, IMessageSubscriber {
+        private MessageBus _Bus;
+
+        void Start() {
+            _Bus = null;
+        }
+
+        public virtual void ReceiveUnhandled(Message msg) {
+            throw new NotImplementedException();
+        }
+
+        public void SetMessageBus(MessageBus Bus) {
+            _Bus = Bus;
+            Bus.Register(this);
+        }
+
+        public void DeregisterHandlers() {
+            _Bus.Deregister(this);
+        }
+
+        protected void Send(Message msg) {
+            MessageSubscriber.SendVia(msg, _Bus, this);
+        }
+
+    }
+
+} // - namespace
